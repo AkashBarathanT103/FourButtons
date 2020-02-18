520af82c2401cdb570a511abcae35f316a49701d
Akash <tjepicdude@gmail.com>
2020-02-18 09:41:53 -0500

    whats up

--- app/src/main/java/com/barathanakash/myapplication/MainActivity.java
+++ app/src/main/java/com/barathanakash/myapplication/MainActivity.java
@@ -37,6 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tV=findViewById(R.id.text_view1);
         sb=findViewById(R.id.seekBar);
+        SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+        //SharedPreferences.Editor editor = sharedPreferences.edit();
+        a=sharedPreferences.getInt("idTV1", a);
+        b=sharedPreferences.getInt("idTV2", b);
+        c=sharedPreferences.getInt("idTV3", c);
+        d=sharedPreferences.getInt("idTV4", d);
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -63,31 +69,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("keyName", "newValue");
         switch(v.getId()){
             case R.id.text_view1:
                 a+=1;
-                Toast.makeText(this, "Button 1 Clicked:"+" "+a+" times", Toast.LENGTH_SHORT).show();
-                editor.putInt("id", a);
+                editor.putInt("idTV1", a);
                 editor.apply();
+                int setting = sharedPreferences.getInt("idTV1", a);
+                Toast.makeText(this, "Button 1 Clicked:"+" "+setting+" times", Toast.LENGTH_SHORT).show();
+
+
                 break;
             case R.id.text_view2:
+
                 b+=1;
-                Toast.makeText(this, "Button 2 Clicked"+" "+b+" times", Toast.LENGTH_SHORT).show();
-                editor.putInt("id", b);
+                editor.putInt("idTV2", b);
                 editor.apply();
+                int setting1 = sharedPreferences.getInt("idTV2", b);
+
+                Toast.makeText(this, "Button 2 Clicked"+" "+setting1+" times", Toast.LENGTH_SHORT).show();
+
                 break;
             case R.id.text_view3:
                 c+=1;
-                Toast.makeText(this, "Button 3 Clicked"+" "+c+" times", Toast.LENGTH_SHORT).show();
-                editor.putInt("id", c);
+                editor.putInt("idTV3", c);
                 editor.apply();
+                int setting2 = sharedPreferences.getInt("idTV3", c);
+                Toast.makeText(this, "Button 3 Clicked"+" "+setting2+" times", Toast.LENGTH_SHORT).show();
+
                 break;
             case R.id.text_view4:
                 d+=1;
-                Toast.makeText(this, "Button 4 Clicked"+" "+d+" times", Toast.LENGTH_SHORT).show();
-                editor.putInt("id", d);
+                editor.putInt("idTV4", d);
                 editor.apply();
+                int setting3 = sharedPreferences.getInt("idTV4", d);
+                Toast.makeText(this, "Button 4 Clicked"+" "+setting3+" times", Toast.LENGTH_SHORT).show();
+
                 break;
 
         }
